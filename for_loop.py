b5f4f66df57eb54d4711c2cdf8513a7225fe2107
bradx32 <bradx32@hotmail.com>
2024-04-06 12:12:43 +1000

    Python Saturday files

---
+++ for_loop.py
@@ -0,0 +1,8 @@
+# For loop
+
+students = [ "John", "Jane", "Mike" ]
+#               1       2       3
+for student in students: # student = John, student = Jane, student = Mike
+    print(student)
+
+    
